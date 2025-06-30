e44c785ec4d2eaaf2389bd023189c76b2c275af2
Maria Zhukova <maria.zhukova@intel.com>
2025-06-30 14:45:52 -0700

    common: extend md impl for host scalars

--- src/common/memory_desc.cpp
+++ src/common/memory_desc.cpp
@@ -31,6 +31,15 @@ using namespace dnnl::impl::utils;
 namespace dnnl {
 namespace impl {
 
+status_t memory_desc_init_host_scalar(
+        memory_desc_t &memory_desc, data_type_t data_type) {
+    memory_desc.ndims = 1;
+    memory_desc.dims[0] = 1;
+    memory_desc.data_type = data_type;
+    memory_desc.format_kind = format_kind::host_scalar;
+    return success;
+}
+
 status_t memory_desc_init_by_tag(memory_desc_t &memory_desc, int ndims,
         const dims_t dims, data_type_t data_type, format_tag_t tag) {
     if (ndims == 0 || tag == format_tag::undef) {
@@ -649,6 +658,17 @@ status_t dnnl_memory_desc_create_with_packed_encoding(
     if (!md) return out_of_memory;
     CHECK(memory_desc_init_by_packed_encoding(
             *md, ndims, dims, data_type, nnz));
+    (*memory_desc) = md.release();
+    return success;
+}
+
+status_t dnnl_memory_desc_create_host_scalar(
+        memory_desc_t **memory_desc, data_type_t data_type) {
+    if (any_null(memory_desc)) return invalid_arguments;
+
+    auto md = utils::make_unique<memory_desc_t>();
+    if (!md) return out_of_memory;
+    CHECK(memory_desc_init_host_scalar(*md, data_type));
     (*memory_desc) = md.release();
     return success;
 }
