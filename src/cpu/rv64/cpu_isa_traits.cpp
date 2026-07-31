962f685bb9d07a3c8e18870bd829d20ae7ebc7fb
Xia Zhuozhao <i@xiazhuozhao.com>
2026-07-31 15:50:50 +0800

    rv64: prefer zvfbfwma ISA

--- src/cpu/rv64/cpu_isa_traits.cpp
+++ src/cpu/rv64/cpu_isa_traits.cpp
@@ -88,6 +88,7 @@ struct isa_info_t {
 };
 
 static isa_info_t get_isa_info_t(void) {
+    if (mayiuse(zvfbfwma)) return isa_info_t(zvfbfwma);
     if (mayiuse(zvfh)) return isa_info_t(zvfh);
     if (mayiuse(v)) return isa_info_t(v);
     return isa_info_t(isa_undef);
